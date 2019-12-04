d3ded77ef649c59f9e216335fb42df1b503a3702
Saeid Ostad <SOstad@kajimausa.com>
2019-12-04 15:54:07 -0500

    remove extra code

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 export * from "./lib/logger";
 
 /** How to use it:  */
-import dotenv from "dotenv";
-import { writeLog, createLogger } from "./lib/logger";
-dotenv.config();
+// import dotenv from "dotenv";
+// import { writeLog, createLogger } from "./lib/logger";
+// dotenv.config();
 
-export async function main() {
-  /** put your code below here */
-  await createLogger();
-  writeLog(`logger is created!`, { stdout: true });
-  console.log(`here is my secret: ${process.env.MY_SECRET}`);
-  return process.env.MY_SECRET;
-}
+// export async function main() {
+//   /** put your code below here */
+//   await createLogger();
+//   writeLog(`logger is created!`, { stdout: true });
+//   console.log(`here is my secret: ${process.env.MY_SECRET}`);
+//   return process.env.MY_SECRET;
+// }
 
-main();
+// main();
