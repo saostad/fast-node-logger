a86edae307376be408c894561659658cb82d0257
Ostad <SOstad@kajimausa.com>
2020-05-18 15:30:40 -0700

    package: updated

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
 /** How to use it:  */
-// import dotenv from "dotenv";
-// import { writeLog, createLogger, Options, logToFile } from "./";
-// dotenv.config();
+import dotenv from "dotenv";
+import { writeLog, createLogger, Options, logToFile } from "./";
+dotenv.config();
 
-// export async function main() {
-//   const options: Options = {
-//     level: "trace",
-//     prettyPrint: { colorize: true, translateTime: "SYS:standard" },
-//   };
-//   const logger = await createLogger(options);
-//   writeLog(`logger is created!`, { stdout: true, level: "info" });
-//   logger.debug("debug log");
-//   logToFile?.info(`here is my secret: ${process.env.MY_SECRET}`);
+export async function main() {
+  const options: Options = {
+    level: "trace",
+    prettyPrint: { colorize: true, translateTime: "SYS:standard" },
+  };
+  const logger = await createLogger(options);
+  writeLog(`logger is created!`, { stdout: true, level: "info" });
+  logger.debug("debug log");
+  logToFile?.info(`here is my secret: ${process.env.MY_SECRET}`);
 
-//   return process.env.MY_SECRET;
-// }
+  return process.env.MY_SECRET;
+}
 
-// main();
+main();
